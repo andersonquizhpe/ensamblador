b49212d7ff36d76e25a5d2c7942a134216e7f405
andersonquizhpe <jhander59@gmail.com>
2020-08-20 19:39:56 -0500

    Cadenas y archivos

---
+++ archivoslectura.asm
@@ -0,0 +1,62 @@
+;Anderson Quizhpe
+;
+;trabajo con archivos
+;2020-08-19
+
+%macro escribir 2
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, %1
+        mov edx, %2
+        int 80h
+%endmacro
+
+section .data
+    msj1 db "Lectura de archivo", 10
+    len1 equ $-msj1
+    
+    archivo db "/home/jhander/sexto/ensamblador/archivo1.txt"
+
+section .bss
+    texto resb 30
+    idarchivo resd 1
+    
+section .text
+    global _start
+
+
+_start:
+    ;****************************************************************************
+    mov eax, 5 		;servicio para crear archivos, trabajar con archivos
+                    ;operacion apertura
+	mov ebx, archivo	; dirección del archivo
+	mov ecx, 1		
+	mov edx, 777h
+	int 80h
+	
+	test eax, eax		; instrucción de comparación realiza la operación lógica “Y” de dos operandos
+    
+	jz salir		; se ejecuta cuando existen errores en el archivo
+	
+	mov dword [idarchivo], eax
+	escribir msj1, len_msj1
+    ;*******************************************************************************
+    mov eax, 3 		;servicio lectura
+	mov ebx, [idarchivo]	; dirección del archivo
+	mov ecx, texto	
+	mov edx, 15
+	int 80h
+
+    escribir texto, 15
+	
+    ;******************************************************************************
+    mov eax, 6 		;servicio para cerrar archivos, trabajar con archivos
+                    ;operacion close
+	mov ebx, [idarchivo]	; dirección del archivo
+	mov ecx, 1	
+	mov edx, 0
+	int 80h
+
+salir:
+	mov eax, 1
+	int 80h
---
+++ numeromenor.asm
@@ -0,0 +1,80 @@
+;Anderson Quizhpe
+;
+;trabajo con cadenas
+;2020-08-19
+
+section .data
+    msj1 db "Ingrese 5 numeros", 10
+    len1 equ $-msj1
+    
+    msj2 db "El numero menor es:", 10
+    len2 equ $-msj2
+
+    arreglo db 0,0,0,0,0
+    len_arreglo equ $-arreglo
+section .bss
+    numero resb 2
+    
+section .text
+    global _start
+_start:
+    mov esi, arreglo
+    mov edi, 0
+    ;****************************
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msj1
+    mov edx, len1
+    int 80h
+    ;*****************************
+leer:
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, numero
+    mov edx, 2
+    int 80h
+
+    mov al,[numero]
+    sub al, '0'
+    
+    mov [esi], al   ; indice 2 (1) es numero 0
+    
+    inc edi
+    inc esi          ;indice del arreglo
+
+    cmp edi, len_arreglo;
+    jb leer
+
+    mov ecx, 0
+    mov bl, 9
+comparacion:
+    mov al, [arreglo+ecx]
+    cmp al, bl
+    ja contador
+    mov bl,al
+
+contador:
+    inc ecx
+    cmp ecx, len_arreglo
+    jb comparacion
+
+imprimir:
+    add bl,'0'
+    mov [numero], bl
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msj2
+    mov edx, len2
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, numero
+    mov edx, 1
+    int 80h
+
+    
+salir:
+    mov eax,1
+    int 80h
