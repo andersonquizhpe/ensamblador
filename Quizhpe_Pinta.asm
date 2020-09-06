6668350db2cf7753cc3039fbe0a5f818cff2aa6e
andersonquizhpe <jhander59@gmail.com>
2020-09-06 13:29:44 -0500

    fin

--- Quizhpe_Pinta.asm
+++ Quizhpe_Pinta.asm
@@ -1,11 +1,8 @@
-;Examen Tema 2
-;
 ;Anderson Quizhpe
 ;
-;Division de dos numeros del 1 al 9
-;2020-08-24
+;Examen de archivos
 
-%macro imprimir 2
+%macro escribir 2
         mov eax, 4
         mov ebx, 1
         mov ecx, %1
@@ -13,70 +10,98 @@
         int 80h
 %endmacro
 
-%macro leer 2
-        mov eax, 3
-        mov ebx, 2
-        mov ecx, %1
-        mov edx, %2
-        int 80h
-%endmacro
-
 section .data
-    msg1 db 10,'Examen Tema 2: Division de dos numeros del 1 al 9', 10
-    len equ $-msg1
-    nlinea db 10,10,0
-	lnlinea equ $-nlinea
+    msj1 db "Leyendo archivo....", 10,10
+    len1 equ $-msj1
+    archivo db "/home/jhander/sexto/ensamblador/datos.txt"
+    
+    arreglo db 0,0,0,0,0,0,0,0,0
+	la equ $-arreglo
+    msj2 db "Guardando datos...", 10,10
+    len2 equ $-msj2
+    archivor db "/home/jhander/sexto/ensamblador/resultados.txt"
 
-    msg2 db 'Ingrese un numero dividendo: '
-    len2 equ $-msg2
-    msg3 db 'Ingrese numero divisor: '
-    len3 equ $-msg3
-    msc db 'Cociente igual: '
-    lenc equ $-msc
-    msr db 10,'Residuo igual:'
-    lenr equ $-msr
 section .bss
-    numero resb 2
-    numero2 resb 2
-    cociente resb 1
-    residuo resb 1
+    texto resb 30
+    idarchivo resd 1
+    idarchivo2 resd 1
+    resultado resb 2
+
+    
 section .text
     global _start
+
 _start:
-    imprimir msg1, len
-    imprimir nlinea, 1
-;**********Ingreso de numeros*************
-    imprimir msg2, len2
-    leer numero, 2
-    imprimir msg3, len3
-    leer numero2,2
+    ;****************************************************************************
+    mov eax, 5 		;servicio para crear archivos, trabajar con archivos operacion apertura
+	mov ebx, archivo	
+	mov ecx, 0		
+	mov edx, 0
+	int 80h
+	
+	test eax, eax
+	jz salir
+	
+	mov dword [idarchivo], eax
+	escribir msj1, len1
+    ;*******************************************************************************
+    mov eax, 3 		;servicio lectura
+	mov ebx, [idarchivo]
+	mov ecx, texto	
+	mov edx, 20
+	int 80h
 
-    mov cx, 0
-    mov al, [numero]
-    mov bl, [numero2]
-    sub al, '0'
-    sub bl, '0'
-    
-;***************Restas suscesivas********************
-restar:
-    sub al, bl
-    inc cx
-    cmp al, bl
-    jae restar
-    jmp resultado
+    escribir texto, 20
+	
+    ;******************************************************************************
+    mov eax, 6 		;close
+	mov ebx, [idarchivo]
+	mov ecx, 0	
+	mov edx, 0
+	int 80h
+;****************************************
+    mov esi, arreglo	;esi-> posc 1er elemento array
+	mov edi, 0
 
-resultado:
-    add al, '0'
-    add cx, '0'
-    mov [cociente], cx
-    mov [residuo], al
-;***********Impresion de resultados******************
-    imprimir msc, lenc
-    imprimir cociente,1
-    imprimir msr, lenr
-    imprimir residuo, 2
-    jmp salir
+leer:
     
+
+	mov [esi], al
+	
+	dec edi
+	dec esi
+
+	cmp edi,la
+	jb leer		
+	mov ecx, 0
+	mov bl, 0
+	
+imprimir:
+    add bl, '0'
+    mov [resultado], bl
+
+	escribir resultado,1
+	;*****************Guardar ARCHIVO**************
+	mov eax, 8
+	mov ebx, archivor
+	mov ecx, 1
+	mov edx, 777h
+	int 80h
+
+	test eax, eax
+	jz salir
+	
+	mov dword [idarchivo2], eax
+
+	escribir msj2, len2
+	
+	;**********se envia al archivo el texto*********
+	mov eax, 4
+	mov ebx, [idarchivo2]
+	mov ecx, resultado
+	mov edx, 1
+	int 80h
+
 salir:
-    mov eax, 1
-    int 80h
+	mov eax, 1
+	int 80h
